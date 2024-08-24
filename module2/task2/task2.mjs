ae114da00b2630d60d6e623242094d944fe5cd25
andrii.semeriak <andrii.semeriak@chisw.com>
2024-08-24 15:23:05 +0300

    add 1 part of  task2

---
+++ module2/task2/task2.mjs
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const __dirname = path.resolve();
+const server = http.createServer(app);
+const io = new Server(server);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
